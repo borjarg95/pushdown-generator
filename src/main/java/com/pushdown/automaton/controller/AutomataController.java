f029387fcc54c745de2e131aba038c545d8532e1
brodrigg <brodrigg@MAD-D221R32.usersad.everis.int>
2018-06-19 00:40:33 +0200

    cambios en properties (endPoints)

--- src/main/java/com/pushdown/automaton/controller/AutomataController.java
+++ src/main/java/com/pushdown/automaton/controller/AutomataController.java
@@ -3,10 +3,14 @@ package com.pushdown.automaton.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.WebEndpoint;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pushdown.automaton.exceptions.AlfabetoNoValidoException;
 import com.pushdown.automaton.model.AutomataPila;
 import com.pushdown.automaton.utils.Utils;
+
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 public class AutomataController {
@@ -36,24 +42,28 @@ public class AutomataController {
 	@Qualifier("generadorAutomataPila")
 	private GeneradorAutomataPila generadorAutomataPila;
  
+	@WebEndpoint
+	@Timed
 	@RequestMapping(value = "/Generate", method = RequestMethod.POST)
-	public ResponseEntity<?> generaAutomata(@RequestBody String entrada) {
-		
+	public ResponseEntity<?> generaAutomata(@RequestBody String entrada, HttpServletRequest request) {
 		String entradaConsulta = Utils.correctorCharEspeciales(entrada);
+		logger.info(request.getLocalName() +" : " +request.getLocalAddr());
 		try {
 			if (automatasGenerados.size() > Utils.TAMANIO_MAPA_AUTOMATAS_GENERADOS)
 				automatasGenerados.clear();
 			AutomataPila automata = new AutomataPila(entradaConsulta, generadorAutomataPila);
 			automata.setIdAutomata(automatasGenerados.size() + 1);
 			automatasGenerados.put(automata.getIdAutomata(), automata);
-			logger.debug("se genera correctamente el automata: "+automata.getIdAutomata() +" "+entradaConsulta);
+			logger.info("La IP: "+request.getLocalName()+ " genera el automata: "+automata.getIdAutomata() +" "+automata);
 			return new ResponseEntity<>(automata, HttpStatus.OK);
 		} catch (Exception e) {
-			logger.error("Error en la generación del automata " + entradaConsulta + " " + e.getMessage());
+			logger.error("La IP: "+request.getLocalAddr()+ " NO genera automata: " + entradaConsulta.replace('\n', ' ') + " " + e.getMessage());
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
 		}
 	}
 
+	@WebEndpoint
+	@Timed
 	@RequestMapping(value = "/CheckWord/{index}/{palabra}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<?> validaPalabra(@PathVariable("index") String index, @PathVariable("palabra") String palabra) {
 		
